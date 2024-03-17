a237868f5b01aeedc7552c7c46cf86bc78a9d6a4
aitbenmoumen <a.aitbenmoumen@gmail.com>
2024-03-17 23:54:44 +0000

    Merge remote-tracking branch 'origin/master'

--- script.js
+++ script.js
@@ -1,30 +1,174 @@
-let api_key = "a8e71c9932b20c4ceb0aed183e6a83bb";
-    let api_url="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
- 
-    const searchbox=document.querySelector(".search input");
-    const searchbtn=document.querySelector(".search button");
-    const weatherIcon=document.querySelector(".weather-icon");
+const currentTemperature = document.getElementById('currentTemp')
+const weatherIcon = document.getElementById('weatherIcon')
+const weatherDescription = document.getElementById('weatherDescription')
+const windSpeed = document.getElementById('wind')
+const windDirection = document.getElementById('windDir')
+const lowestToday = document.getElementById('lowestToday')
+const highestToday = document.getElementById('highestToday')
+const pressure = document.getElementById('pressure')
+const humidity = document.getElementById('humidity')
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
+const sunriseRelative = document.getElementById('sunriseRelative')
+const sunsetRelative = document.getElementById('sunsetRelative')
+const userLocation = document.getElementById('location')
+const time = document.getElementById('time')
+const date = document.getElementById('date')
+const searchInput = document.getElementById('searchInput')
 
-    async function checkWeather(city){
-        const response = await fetch(api_url + city + `&appid=${api_key}`);
-        const data = await response.json();
-        console.log(data);
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
-        document.querySelector(".wind").innerHTML = data.wind.speed+"km/h";
-        if(data.weather[0].main=="Clouds"){
-            weatherIcon.src="images/clouds.png";
-        }else if(data.weather[0].main=="Clear"){
-            weatherIcon.src="images/clear.png";
-        }else if(data.weather[0].main=="Rain"){
-            weatherIcon.src="images/rain.png";
-        }else if(data.weather[0].main=="Drizzle"){
-            weatherIcon.src="images/drizzle.png";
-        }else if(data.weather[0].main=="Mist"){
-            weatherIcon.src="images/mist.png";
-        }
-    }
-    searchbtn.addEventListener("click",()=>{
-        checkWeather(searchbox.value);
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const getWeatherData = async () => {
+  // Use the try-catch block to handle errors
+  try {
+    // Create a const that stores the user input from the searchbar or defaults back to 'Los Angeles' if left blank
+    const city = searchInput.value || 'Los Angeles'
+    const currentWeather = new Promise(async (resolve, reject) => {
+      try {
+        const weatherApiData = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8109965e7254a469d08a746e8b210e1e&units=imperial`,
+        )
+
+        resolve(await weatherApiData.json())
+      } catch (error) {
+        reject()
+      }
     })
+
+    const forecast = new Promise(async (resolve, reject) => {
+      try {
+        const forecastApiData = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=8109965e7254a469d08a746e8b210e1e&units=imperial&cnt=10`,
+        )
+
+        resolve(await forecastApiData.json())
+      } catch (error) {
+        reject()
+      }
+    })
+    const data = await Promise.all([currentWeather, forecast])
+    updateDom(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getDirection = deg => {
+  switch (true) {
+    case deg < 22.5:
+      return 'N'
+    case deg < 67.5:
+      return 'NE'
+    case deg < 112.5:
+      return 'E'
+    case deg < 157.5:
+      return 'SE'
+    case deg < 202.5:
+      return 'S'
+    case deg < 247.5:
+      return 'SW'
+    case deg < 292.5:
+      return 'W'
+    case deg < 337.5:
+      return 'NW'
+  }
+}
+
+
+const updateDom = data => {
+  console.log('🔥 updating', data)
+  currentTemperature.innerText = data[0].main.temp.toFixed(1)
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`
+
+  weatherDescription.innerText = data[0].weather[0].main
+
+  windSpeed.innerText = data[0].wind.speed.toFixed(1)
+
+  windDirection.innerText = getDirection(data[0].wind.deg)
+
+  lowestToday.innerText = Math.round(data[0].main.temp_min)
+
+  highestToday.innerText = Math.round(data[0].main.temp_max)
+
+  pressure.innerText = data[0].main.pressure
+
+  humidity.innerText = data[0].main.humidity
+
+  const sunriseTs = new Date(data[0].sys.sunrise * 1000)
+  const sunsetTs = new Date(data[0].sys.sunset * 1000)
+
+  sunrise.innerText = sunriseTs.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+  })
+  sunset.innerText = sunsetTs.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+  })
+
+  sunriseRelative.innerText = timeago.format(sunriseTs)
+  sunsetRelative.innerText = timeago.format(sunsetTs)
+
+  
+  userLocation.innerText = data[0].name
+
+  time.innerText = new Date(Date.now()).toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+  })
+
+  date.innerText = new Date(Date.now()).toLocaleString('en-US', {
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  })
+
+  renderChart(data[1].list)
+}
+
+//function that renders the chart
+const renderChart = data => {
+  // Store the DOM element that will hold the chart
+  const myChart = echarts.init(document.getElementById('chart'))
+
+  const option = {
+    legend: {
+      data: ['temperature'],
+    },
+    tooltip: {},
+    xAxis: {
+      data: data.map(item => item.dt_txt),
+    },
+    yAxis: {},
+    series: [
+      {
+        type: 'line',
+        smooth: true,
+        areaStyle: {
+          opacity: 0.5,
+        },
+        data: data.map(item => item.main.temp),
+      },
+    ],
+  }
+
+  // Using the given function from the documentation
+  myChart.setOption(option)
+}
+
+// Call the getWeatherData function
+getWeatherData()
